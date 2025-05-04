55d5585021c7c47a4708a98daf4091514d5f19ca
Atahanka <atahanka@gmail.com>
2025-05-04 16:12:50 +0200

    .

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from './three.module.js';
-import { OrbitControls } from './OrbitControls.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
 
 console.log("Three.js loaded");
 
